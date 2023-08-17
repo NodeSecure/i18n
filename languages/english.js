c4c1bdf0d1d3155f1c10a30cfcf558d1247824d1
fabnguess <fabnguess@gmail.com>
2023-08-17 13:43:31 +0000

    refactor(languages): Add semver token

--- languages/english.js
+++ languages/english.js
@@ -117,7 +117,7 @@ const ui = {
 
 const warnings = {
   disable_scarf: "This dependency could collect data against your will so think to disable it with the env var: SCARF_ANALYTICS",
-  keylogging: "This dependency can retrieve your keyboard and mouse inputs. It can be used for 'keylogging' attacks/malwares.",
+  keylogging: "This dependency can retrieve your keyboard and mouse inputs. It can be used for 'keylogging' attacks/malwares."
 };
 
 const sast_warnings = {
